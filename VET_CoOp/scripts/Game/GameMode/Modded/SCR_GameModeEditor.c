443100d71a3dd7ec180b9d64d3cf6ceb85eed41b
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-05-26 20:42:57 +0200

    Add attribute for preventing the zeus to go into camera after death
    Should be more mod compatible. Retains default behaviour by default and is changed in my own prefab.

--- VET_CoOp/scripts/Game/GameMode/Modded/SCR_GameModeEditor.c
+++ VET_CoOp/scripts/Game/GameMode/Modded/SCR_GameModeEditor.c
@@ -1,7 +1,16 @@
 modded class SCR_GameModeEditor : SCR_BaseGameMode
 {
+	[Attribute(defvalue: "1", category: "Game Mode: Editor", desc: "Open the editor automatically after death when playing solo.")]
+	protected bool m_openEditorAfterDeath;
+	
 	override protected void OnPlayerTerminated(int playerId)
 	{
-		Print("OnPlayerTerminated override, prevent player from being forced into Game Master camera", LogLevel.DEBUG);
+		if (!m_openEditorAfterDeath)
+		{
+			Print("OnPlayerTerminated override, prevent player from being forced into Game Master camera", LogLevel.DEBUG);
+			return;
+		}
+		
+		super.OnPlayerTerminated(playerId);
 	}
 }
