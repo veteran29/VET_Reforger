7ea1a027703ffd207489776508bd06bd27ba18ce
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-05-30 23:45:38 +0200

    Add drawing of Spawned Waypoints to SCR_AIGroup

--- VET_Core/scripts/Game/Entities/Modded/SCR_AIGroup.c
+++ VET_Core/scripts/Game/Entities/Modded/SCR_AIGroup.c
@@ -9,4 +9,35 @@ modded class SCR_AIGroup: ChimeraAIGroup
 	{
 		m_bSpawnImmediately = spawnImmediately;
 	}
-};
+	
+#ifdef WORKBENCH
+	
+	override void _WB_AfterWorldUpdate(float timeSlice)
+	{
+		super._WB_AfterWorldUpdate(timeSlice);
+		
+		// draw spawned waypoints positions
+		foreach (SCR_WaypointPrefabLocation spawnedWaypoint : m_aSpawnedWaypoints)
+		{
+			float radius;
+			if (spawnedWaypoint.m_WPRadiusOverride)
+			{
+				radius = spawnedWaypoint.m_WPRadiusOverride;
+			}
+			else
+			{
+				Resource wpRes = Resource.Load(spawnedWaypoint.m_WPPrefabName);
+				wpRes.GetResource().ToBaseContainer().Get("CompletionRadius", radius);
+			}
+			
+			vector waypointPosition = spawnedWaypoint.m_WPWorldLocation;
+			//Shape shape = Shape.CreateSphere(COLOR_YELLOW, ShapeFlags.ONCE | ShapeFlags.NOOUTLINE, waypointPosition, radius);
+			Color color = Color.FromRGBA(0, 0, 255, 128);
+			Shape shape = Shape.CreateCylinder(color.PackToInt(), ShapeFlags.ONCE | ShapeFlags.NOOUTLINE | ShapeFlags.TRANSP, waypointPosition, radius, 2);
+			Shape arrow = Shape.CreateArrow(GetOrigin() + "0 3 0", waypointPosition + "0 1 0", 1.5, COLOR_YELLOW, ShapeFlags.ONCE);
+		}		
+	}
+	
+#endif
+	
+}
